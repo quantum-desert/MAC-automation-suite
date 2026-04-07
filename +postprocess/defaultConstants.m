230c3a7059217a2718648c9828173c8a83491a13
visuttha-m <visuttha@umich.edu>
2026-04-07 14:15:04 -0400

    update modulation frequency

--- +postprocess/defaultConstants.m
+++ +postprocess/defaultConstants.m
@@ -2,8 +2,8 @@ function constants = defaultConstants()
 % postprocess.defaultConstants  Default constants copied from the original script.
 
 constants = struct;
-constants.Rb = 8e3; % bit rate (Hz) PN15
-% constants.Rb = 16e3; % bit rate (Hz), used for determinstic mod
+% constants.Rb = 8e3; % bit rate (Hz) PN15
+constants.Rb = 16e3; % bit rate (Hz), used for determinstic mod
 constants.fd = 32e3; % dither frequency (Hz)
 constants.filtering = 1; % filter flag
 constants.S = 1.23; % PD responsivity
